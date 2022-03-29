a4b59dc82cb1019068c4c02f52a4823446a6c0b9
chlucas324 <chlucas324@gmail.com>
2022-03-28 20:30:06 -0400

    add due date handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+// get date from task element
+var date = $(taskEl).find("span").text().trim();
+// convert to moment object at 5:00 pm
+var time = moment(date, "L").set("hour", 17);
+// remove any old classes from element
+$(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+//apply new class if task is near/over due date
+if(moment().isAfter(time)) {
+  $(taskEl).addClass("list-group-item-danger");
+}
+else if(Math.abs(moment().diff(time, "days")) <= 2) {
+  $(taskEl).addClass("list-group-item-warning");
+}
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,9 +29,16 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
 
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -114,7 +137,6 @@ $("#trash").droppable({
   }
 });
 
-
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -205,6 +227,15 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      //when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
@@ -231,6 +262,9 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+
+    //pass task's <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
